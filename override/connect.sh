0cb2cce01f059a13214585d1f044750c888cc8fe
ocrossi <zhdoctor971@gmail.com>
2023-09-04 09:02:26 +0200

    override lets go

--- override/connect.sh
+++ override/connect.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 pn=4242
-ip="192.168.243.130"
-
+ip=192.168.1.28
 echo "enter level number, ranging from 0 to 9"
 read input
 
-re='^[0-9]+$'
+#re='^[0-9]'
 
-while ! [[ $yournumber =~ $re ]] && [ $input -gt 9 -o $input -lt 0 ]
+while ! [ $input -lt 9 -o $input -gt 0 ]
 do
 	echo "wrong input"
+	echo " gimme input $input"
 	read input
 done
 
-level="level$input"
+level="level0${input}"
+
 num=$((input + 1))
-
-
 pass=`sed "${num}q;d" ./pass`
 
+# echo "pass $pass"
+# echo "level $level"
+# echo "num $num"
 sshpass -p $pass ssh $level@$ip -p $pn "chmod 777 ."
 sshpass -p $pass scp -P $pn ./gdbinit  $level@$ip:/home/user/$level/.gdbinit
 sshpass -p $pass ssh $level@$ip -p $pn
